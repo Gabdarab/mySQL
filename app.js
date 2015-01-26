f06319619c079ea52070a5725b7fbcc8bbaa9b8e
Gabdarab <gdarvasi@gmail.com>
2015-01-26 22:48:50 +0100

    http and mysql

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 var http = require('http');
-var url = require('url');
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var mysql = require('mysql');
+
+var pool = mysql.createPool({
   	host     : 'localhost',
   	user     : 'ecm-server',
   	password : 'mypass',
@@ -10,49 +10,25 @@ var connection = mysql.createConnection({
 });
 
 var server = http.createServer(function (req,res) {
-	var myjson;
+    
 
-	req.on('end', function (){
-		connection.connect(function(err) {
-  			if (err) {
-    			console.error('error connecting: ' + err.stack);
-    			return;
-  			}
-			console.log('connected as id ' + connection.threadId);
-		});
+    pool.getConnection(function(err, connection){
+        if (err){
+            throw err;
+        }else{
+            connection.query("select * from pet where owner = 'Harold'", function(err, rows){
+                if (err) {
+                    throw err;
+                }else{
+                    console.log("Query finished: " + rows[0].name);
+                    res.end("Query finished: " + rows[0].name);
+                }
+            })
+            connection.release(console.log("Connection released!"));
+        };
+    });  
 
-		connection.query("select * from pet where owner = 'Harold'", function(err, rows, fields) {
-			if (err) throw err;
-  			var aux = JSON.stringify(rows);
-  			var aux2 = JSON.parse(aux);
-  			myjson = aux2;
-
-		});
-		connection.end();
-	})
-
-	res.end("Response: "+ myjson[0].owner);
 })
 
 server.listen(8000);
 console.log("Server is running.");
-
-
-/*
-connection.connect(function(err) {
-  	if (err) {
-    	console.error('error connecting: ' + err.stack);
-    return;
-  }
-console.log('connected as id ' + connection.threadId);
-});
-
-connection.query("select * from pet where owner = 'Harold'", function(err, rows, fields) {
-	if (err) throw err;
-  	var aux = JSON.stringify(rows);
-  	var aux2 = JSON.parse(aux);
-  	console.log(aux2[1].name);
-
-});
-connection.end();
-*/
